a4cc8c828027264388f6f2ffdd007b382ab3b299
Cheber <sdgfqasdf46@yandex.ru>
2021-03-26 01:57:43 +0300

    version1

--- src/main/java/ru/geekbraines/exception/MyArraySizeException.java
+++ src/main/java/ru/geekbraines/exception/MyArraySizeException.java
@@ -2,4 +2,8 @@ package ru.geekbraines.exception;
 
 public class MyArraySizeException extends RuntimeException {
 
+    @Override
+    public String getMessage() {
+        return "Размер массива должен быть 4х4";
+    }
 }
